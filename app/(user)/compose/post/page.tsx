d4ba29d3c161e32ec412d579a39302f818c4740a
Jubril <jubriloyebamiji@gmail.com>
2026-02-17 12:44:29 -0800

    pages route created

---
+++ app/(user)/compose/post/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ComposePostPage() {
+  return <div>ComposePostPage</div>;
+}
---
+++ app/(user)/i/premium_sign_up/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PremiumSignUp() {
+  return <div>PremiumSignUp</div>;
+}
