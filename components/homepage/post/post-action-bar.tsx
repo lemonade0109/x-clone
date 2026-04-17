a7d7a3cc18211b7af60c58174911bd7d3ecd69a5
Jubril <jubriloyebamiji@gmail.com>
2026-04-17 06:03:56 -0700

    get-all-post-action updated.

---
+++ components/homepage/post/post-action-bar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostActionBar = () => {
+  return <div>PostActionBar</div>;
+};
+
+export default PostActionBar;
--- lib/actions/post-actions/get-all-posts-action.ts
+++ lib/actions/post-actions/get-all-posts-action.ts
@@ -1,16 +1,58 @@
 "use server";
 
+import { auth } from "@/auth";
 import { db } from "@/db/db";
 
 export const getAllPostsAction = async () => {
+  const session = await auth();
+  const email = session?.user.email;
+
+  const currentUser = email
+    ? await db.user.findUnique({ where: { email }, select: { id: true } })
+    : null;
+
   const posts = await db.post.findMany({
     orderBy: {
       createdAt: "desc",
     },
     include: {
       author: true,
+      _count: {
+        select: {
+          likes: true,
+          comments: true,
+          reposts: true,
+          bookmarks: true,
+        },
+      },
+      likes: currentUser ? { where: { authorId: currentUser.id } } : false,
+      reposts: currentUser ? { where: { authorId: currentUser.id } } : false,
+      bookmarks: currentUser ? { where: { authorId: currentUser.id } } : false,
     },
   });
 
-  return posts;
+  return posts.map((post) => ({
+    ...post,
+    likesCount: post._count.likes,
+    commentsCount: post._count.comments,
+    repostsCount: post._count.reposts,
+    bookmarksCount: post._count.bookmarks,
+    isLiked: post.likes.length > 0,
+    isReposted: post.reposts.length > 0,
+    isBookmarked: post.bookmarks.length > 0,
+  }));
 };
+
+// Without like, comment, repost and bookmark
+// export const getAllPostsAction = async () => {
+//   const posts = await db.post.findMany({
+//     orderBy: {
+//       createdAt: "desc",
+//     },
+//     include: {
+//       author: true,
+//     },
+//   });
+
+//   return posts;
+// };
